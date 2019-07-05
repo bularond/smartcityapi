28c5ce225358a6ef5f3be7bd3108d6846adee673
bularond <bualarond@gmail.com>
2019-07-05 13:10:28 +0300

    Fix mailer

--- server/mailer/mailer.py
+++ server/mailer/mailer.py
@@ -1,10 +1,11 @@
-import mailer.smtp_mail
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from .smtp_mail import SMTP
 import pymongo
 
 
-
 def send_messages(message_list):
-    smtp_obj = smtp_mail.SMTP("no-repy@gradintegra.com", "|kko)8s2K(WT6u!m")        #Create smtp ojbect SMTP("login", "password")
+    smtp_obj = SMTP("no-repy@gradintegra.com", "|kko)8s2K(WT6u!m")        #Create smtp ojbect SMTP("login", "password")
     month = {
         1:"января",
         2:"февраля",
@@ -23,11 +24,12 @@ def send_messages(message_list):
         "energy": "отключение электричества"
     }
     for man in message_list:
-        message = f"""Уважаемый {man["FIO"]}!
-\nХотим сообщить, что {str(man["begin"].day)} {str(month[man["begin"].month])} на улице {doc_User["street"]} планируется {types[man["type"]]}."""
+        message = ''
         smtp_obj.send_message(man["mail"], message, "Отключение электроэнергии")
 
 
-
-
-#smtp_obj.send_message("djalil6789@gmail.com", "qweqwtrt", "wrtjj")              #Send message send_message("destination mail", "message0", "Subject")
+if __name__ == "__main__":
+    smtp_obj = SMTP("no-repy@gradintegra.com", "|kko)8s2K(WT6u!m")
+    message = "фыв123"
+    email = 'bularond@gmail.com'
+    smtp_obj.send_message(email, message, "123")
--- server/mailer/smtp_mail.py
+++ server/mailer/smtp_mail.py
@@ -15,15 +15,15 @@ class SMTP:
         dest_email = to_mail
         subject = subject
         email_text = message
-        message = 'From: {}\nTo: {}\nSubject: {}\n\n{}'.format(email,
+        '''message = 'From: {}\nTo: {}\nSubject: {}\n\n{}'.format(email,
                                                                dest_email,
                                                                subject,
-                                                               email_text)
+                                                               email_text)'''
 
         server = smtp.SMTP_SSL('smtp.yandex.com')
         server.set_debuglevel(1)
         server.ehlo(email)
         server.login(email, password)
         #server.auth_plain()
-        server.sendmail(email, dest_email, message)
+        server.sendmail(email, dest_email, message.encode('utf-8'))
         server.quit()
